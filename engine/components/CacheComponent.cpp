0e5e89cced5c666a514e9d418c7dd3273410a564
Lily Botha <lilybotha06@gmail.com>
2026-03-04 21:57:40 +0100

    Finished exercises and added templated ThrashThatCache function

--- engine/components/CacheComponent.cpp
+++ engine/components/CacheComponent.cpp
@@ -4,8 +4,6 @@
 
 #include "CacheComponent.h"
 
-#include <algorithm>
-#include <chrono>
 #include <print>
 
 #include "GameObject.h"
@@ -17,48 +15,42 @@ dae::CacheComponent::CacheComponent(GameObject* pOwner, int bufferSize)
     , m_pUIComponent1(pOwner->AddComponent<UIComponent>(
           [this](GameObject*) mutable
           {
-              static int sampleCount = 1;
-              static bool plotExists{};
+              static int sampleCount = 30;
 
               if(ImGui::Begin("Exercise 1"))
               {
                   ImGui::InputInt("samples", &sampleCount);
                   if(ImGui::Button("Thrash the Cache"))
-                  {
-                      plotExists = true;
                       RunExercise1(sampleCount);
-                  }
               }
 
-              if(plotExists)
-              {
-                  ImPlot::BeginPlot("Durations");
-                  ImPlot::PlotLine<uint32_t>("", m_durations1.data(), static_cast<int>(m_durations1.size()), 10, 0);
-                  ImPlot::EndPlot();
-              }
+              ImPlot::SetNextAxesToFit();
+              ImPlot::BeginPlot("Durations with integers");
+              ImPlot::PlotLine<uint32_t>("", m_durations1.data(), static_cast<int>(m_durations1.size()), 100, 0);
+              ImPlot::EndPlot();
 
               ImGui::End();
           }))
     , m_pUIComponent2(pOwner->AddComponent<UIComponent>(
           [this](GameObject*) mutable
           {
-              static int sampleCount = 0;
-              static bool plotExists{};
+              static int sampleCount = 30;
 
               if(ImGui::Begin("Exercise 2"))
               {
                   ImGui::InputInt("samples", &sampleCount);
-                  if(ImGui::Button("Thrash the Cache"))
+                  if(ImGui::Button("Thrash the Cache with GameObject3D"))
                   {
-                      plotExists = true;
                       RunExercise2(sampleCount);
                   }
-              }
+                  if(ImGui::Button("Thrash the Cache with GameObject3DAlt"))
+                  {
+                      RunExercise3(sampleCount);
+                  }
 
-              if(plotExists)
-              {
-                  ImPlot::BeginPlot("Durations");
-                  ImPlot::PlotLine<uint32_t>("", m_durations2.data(), static_cast<int>(m_durations2.size()), 10, 0);
+                  ImPlot::SetNextAxesToFit();
+                  ImPlot::BeginPlot("Durations with GameObject3D(Alt)");
+                  ImPlot::PlotLine<uint32_t>("", m_durations2.data(), static_cast<int>(m_durations2.size()), 100, 0);
                   ImPlot::EndPlot();
               }
 
@@ -69,6 +61,7 @@ dae::CacheComponent::CacheComponent(GameObject* pOwner, int bufferSize)
     std::ranges::fill(m_buffer1, 1);
 
     m_buffer2.resize(bufferSize);
+    m_buffer3.resize(bufferSize);
 }
 
 void dae::CacheComponent::Update(float) {}
@@ -76,53 +69,17 @@ void dae::CacheComponent::Update(float) {}
 void dae::CacheComponent::RunExercise1(int sampleCount)
 {
     std::println("Running exercise 1 with {} samples", sampleCount);
-    m_durations1.resize(10);
-    std::ranges::fill_n(m_durations1.begin(), 10, 0);
-
-    for(int sampleIndex{}; sampleIndex < sampleCount; ++sampleIndex)
-    {
-        int durationIndex{};
-        for(int stepsize = 1; stepsize < 1024; stepsize *= 2)
-        {
-            auto start = std::chrono::high_resolution_clock::now();
-            for(size_t i{}; i < m_buffer1.size(); i += stepsize)
-            {
-                m_buffer1[i] *= stepsize + 1;
-            }
-            auto end = std::chrono::high_resolution_clock::now();
-            auto duration = static_cast<uint32_t>(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
-            m_durations1.at(durationIndex) += duration;
-            ++durationIndex;
-        }
-    }
-
-    // Divide to get average
-    std::ranges::transform(m_durations1, m_durations1.begin(), [sampleCount](const auto& duration){ return duration / sampleCount; });
+    ThrashThatCache(sampleCount, m_buffer1, m_durations1);
 }
 
 void dae::CacheComponent::RunExercise2(int sampleCount)
 {
     std::println("Running exercise 2 with {} samples", sampleCount);
+    ThrashThatCache(sampleCount, m_buffer2, m_durations2);
+}
 
-    m_durations2.resize(10);
-    std::ranges::fill_n(m_durations2.begin(), 10, 0);
-
-    for(int sampleIndex{}; sampleIndex < sampleCount; ++sampleIndex)
-    {
-        int durationIndex{};
-        for(int stepsize = 1; stepsize < 1024; stepsize *= 2)
-        {
-            auto start = std::chrono::high_resolution_clock::now();
-            for(size_t i{}; i < m_buffer2.size(); i += stepsize)
-            {
-                m_buffer2[i].ID *= stepsize + 1;
-            }
-            auto end = std::chrono::high_resolution_clock::now();
-            auto duration = static_cast<uint32_t>(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
-            m_durations2.at(durationIndex) += duration;
-            ++durationIndex;
-        }
-    }
-
-    std::ranges::transform(m_durations2, m_durations2.begin(), [sampleCount](const auto& duration){ return duration / sampleCount; });
+void dae::CacheComponent::RunExercise3(int sampleCount)
+{
+    std::println("Running exercise 3 with {} samples", sampleCount);
+    ThrashThatCache(sampleCount, m_buffer3, m_durations2);
 }
--- engine/components/CacheComponent.h
+++ engine/components/CacheComponent.h
@@ -6,6 +6,8 @@
 #define ENGINE_CACHECOMPONENT_H
 #include <cstdint>
 #include <vector>
+#include <algorithm>
+#include <chrono>
 
 #include "Component.h"
 
@@ -33,8 +35,45 @@ private:
         int ID{};
     };
 
+    class GameObject3DAlt
+    {
+    public:
+        Transform* transform = new Transform();
+        int ID{};
+    };
+
+    template<class T>
+    void ThrashThatCache(int sampleCount, std::vector<T>& buffer, std::vector<uint32_t>& durations)
+    {
+        durations.clear();
+        durations.resize(10);
+
+        for(int sampleIndex{}; sampleIndex < sampleCount; ++sampleIndex)
+        {
+            int durationIndex{};
+            for(int stepsize = 1; stepsize < 1024; stepsize *= 2)
+            {
+                auto start = std::chrono::high_resolution_clock::now();
+                for(size_t i{}; i < buffer.size(); i += stepsize)
+                {
+                    if constexpr( requires{ T::ID; })
+                        buffer[i].ID *= stepsize + 1;
+                    else
+                        buffer[i] *= stepsize + 1;
+                }
+                auto end = std::chrono::high_resolution_clock::now();
+                auto duration = static_cast<uint32_t>(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
+                durations.at(durationIndex) += duration;
+                ++durationIndex;
+            }
+        }
+
+        // Divide to get average
+        std::ranges::transform(m_durations1, m_durations1.begin(), [sampleCount](const auto& duration){ return duration / sampleCount; });
+    }
     void RunExercise1(int sampleCount);
     void RunExercise2(int sampleCount);
+    void RunExercise3(int sampleCount);
 
     UIComponent* m_pUIComponent1{};
     UIComponent* m_pUIComponent2{};
@@ -43,6 +82,7 @@ private:
     std::vector<uint32_t> m_durations1;
 
     std::vector<GameObject3D> m_buffer2;
+    std::vector<GameObject3DAlt> m_buffer3;
     std::vector<uint32_t> m_durations2;
 };
 
