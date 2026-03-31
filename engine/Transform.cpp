cffad6fc7ea92d165dccc10c51da48659f6ed9a3
Lily Botha <lilybotha06@gmail.com>
2026-03-31 15:16:48 +0200

    Added scale to transform

--- engine/Transform.cpp
+++ engine/Transform.cpp
@@ -2,6 +2,9 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/matrix_decompose.hpp>
+
 #include "GameObject.h"
 
 dae::Transform::Transform(float x, float y, float z, GameObject* parent)
@@ -64,6 +67,15 @@ glm::vec3 dae::Transform::GetWorldPosition()
     return m_worldPosition;
 }
 
+void dae::Transform::SetScale(float x, float y, float z)
+{
+    m_localScale.x = x;
+    m_localScale.y = y;
+    m_localScale.z = z;
+
+    m_pOwner->SetDirty();
+}
+
 glm::mat4 dae::Transform::GetWorldMatrix()
 {
     if(isDirty)
@@ -72,17 +84,37 @@ glm::mat4 dae::Transform::GetWorldMatrix()
     return m_worldMatrix;
 }
 
+glm::vec2 dae::Transform::GetLocalScale() const
+{
+    return m_localScale;
+}
+
+glm::vec2 dae::Transform::GetWorldScale()
+{
+    if(isDirty)
+        UpdateWorldMatrix();
+
+    return m_worldScale;
+}
+
 void dae::Transform::UpdateWorldMatrix()
 {
     glm::mat4 rotationMatrix(1.0f);
-    rotationMatrix = glm::rotate(rotationMatrix, m_rotation.z, glm::vec3(0, 0, 1));  // roll
-    rotationMatrix = glm::rotate(rotationMatrix, m_rotation.x, glm::vec3(1, 0, 0));  // pitch
-    rotationMatrix = glm::rotate(rotationMatrix, m_rotation.y, glm::vec3(0, 1, 0));  // yaw
+    rotationMatrix = glm::rotate(rotationMatrix, m_localRotation.z, glm::vec3(0, 0, 1));  // roll
+    rotationMatrix = glm::rotate(rotationMatrix, m_localRotation.x, glm::vec3(1, 0, 0));  // pitch
+    rotationMatrix = glm::rotate(rotationMatrix, m_localRotation.y, glm::vec3(0, 1, 0));  // yaw
 
     const glm::mat4 localMatrix =
-        glm::translate(glm::mat4(1.0f), m_localPosition) * rotationMatrix * glm::scale(glm::mat4(1.0f), m_scale);
+        glm::translate(glm::mat4(1.0f), m_localPosition) * rotationMatrix * glm::scale(glm::mat4(1.0f), m_localScale);
 
     m_worldMatrix = m_pOwner->GetParentWorldMatrix() * localMatrix;
+
+    // Decompose the world matrix down to it's individual components
+    glm::vec3 skew;
+    glm::vec4 perspective;
+    glm::quat worldRotationQuat;
+    glm::decompose(m_worldMatrix, m_worldScale, worldRotationQuat, m_localPosition, skew, perspective);
+    m_worldRotation = glm::eulerAngles(worldRotationQuat);
 
     isDirty = false;
 }
--- engine/Transform.h
+++ engine/Transform.h
@@ -6,33 +6,46 @@ namespace dae
 class GameObject;
 
 class Transform final
-	{
-	public:
-	    explicit Transform(float x, float y, float z = 0, GameObject* parent = nullptr);
-        explicit Transform(const Transform& transform, GameObject* owner);
+{
+public:
+    explicit Transform(float x, float y, float z = 0, GameObject* parent = nullptr);
+    explicit Transform(const Transform& transform, GameObject* owner);
 
-		void SetLocalPosition(float x, float y, float z = 0);
-		void SetLocalPosition(const glm::vec3& position);
-	    void ChangeLocalPosition(float x, float y, float z = 0);
-	    void ChangeLocalPosition(const glm::vec3& delta);
+    // Position
+    void SetLocalPosition(float x, float y, float z = 0);
+    void SetLocalPosition(const glm::vec3& position);
+    void ChangeLocalPosition(float x, float y, float z = 0);
+    void ChangeLocalPosition(const glm::vec3& delta);
+    [[nodiscard]] glm::vec3 GetLocalPosition() const;
+    [[nodiscard]] glm::vec3 GetWorldPosition();
 
-        [[nodiscard]] glm::vec3 GetLocalPosition() const;
-	    [[nodiscard]] glm::vec3 GetWorldPosition();
-        [[nodiscard]] glm::mat4 GetWorldMatrix();
+    // Rotation
+    // TODO: Add once I need it :P
+
+    // Scale
+    void SetScale(float x, float y, float z = 1);
+    [[nodiscard]] glm::vec2 GetLocalScale() const;
+    [[nodiscard]] glm::vec2 GetWorldScale();
 
 
-	    void UpdateWorldMatrix();
-	    void ApplyInverseTransform(const Transform& parentTransform);
+    // World
+    [[nodiscard]] glm::mat4 GetWorldMatrix();
+    void UpdateWorldMatrix();
+    void ApplyInverseTransform(const Transform& parentTransform);
 
-        bool isDirty{ true };
-	private:
-	    GameObject* m_pOwner{};
+    bool isDirty{ true };
 
-	    glm::mat4 m_worldMatrix{ 1.f };
-	    glm::vec3 m_rotation{ 0.f };
+private:
+    GameObject* m_pOwner{};
 
-	    glm::vec3 m_scale{ 1.f };
-		glm::vec3 m_localPosition{ 0.f };
-		glm::vec3 m_worldPosition{ 0.f };
-	};
-}
+    glm::mat4 m_worldMatrix{ 1.f };
+
+    glm::vec3 m_localRotation{ 0.f };
+    glm::vec3 m_localScale{ 1.f };
+    glm::vec3 m_localPosition{ 0.f };
+
+    glm::vec3 m_worldRotation{ 0.f };
+    glm::vec3 m_worldScale{ 1.f };
+    glm::vec3 m_worldPosition{ 0.f };
+};
+}  // namespace dae
