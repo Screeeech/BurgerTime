e0d04f0f6f511ca8f67cbb76596b82a52e9e50ac
Lily Botha <lilybotha06@gmail.com>
2026-04-27 17:42:27 +0200

    Remove Engine directory before adding it as a submodule

--- Engine/cmake/emscripten/Findharfbuzz.cmake
+++
@@ -1,7 +0,0 @@
-# Fakes Findharfbuzz.cmake for Emscripten
-set(harfbuzz_FOUND TRUE)
-set(harfbuzz_VERSION "3.0.0")
-
-if(NOT TARGET harfbuzz::harfbuzz)
-  add_library(harfbuzz::harfbuzz INTERFACE IMPORTED)
-endif()
